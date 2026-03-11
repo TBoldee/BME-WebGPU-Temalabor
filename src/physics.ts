9d2c3df9b6071474ef7b22d1eadbe1dc128f2f1c
TBoldee <turosboldizsar@gmail.com>
2026-03-11 18:33:02 +0100

    collision detection elkezdve: checkCollision és getCollidedRects

--- src/physics.ts
+++ src/physics.ts
@@ -1,10 +1,36 @@
-import type {Level} from "./level.ts";
+import type {Level, Rect} from "./level.ts";
+import type {Player} from "./player.ts";
 const canvas = document.querySelector('canvas') as HTMLCanvasElement;
 //const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
-export function applyPhysics(level: Level) {
+export function applyPhysics(level: Level): void {
     const player = level.player;
-    if (player.y < canvasHeight - player.h){
-        player.y = Math.min(canvasHeight - player.h, player.y + 20);
+    if (player.y > canvasHeight - player.h) return;
+
+    player.y = Math.min(canvasHeight - player.h, player.y + 20);
+    let collidedRects = getCollidedRects(level);
+    if (collidedRects.length !== 0) resolveCollisions(collidedRects, player);
+}
+
+
+function getCollidedRects(level: Level){
+    const collidedRects: Rect[] = [];
+    for (const rect of level.rects) {
+        if (checkCollision(rect, level.player)){
+            collidedRects.push(rect);
+        }
     }
+    return collidedRects;
+}
+function checkCollision(rect: Rect, player: Player): boolean {
+    if (player.x + player.w < rect.x || player.x > rect.x + rect.w){
+        if (player.y + player.h < rect.y || player.y > rect.y + rect.h){
+            return false;
+        }
+    }
+    return true;
+}
+
+function resolveCollisions(collidedRects: Rect[], player: Player) {
+    
 }
--- src/player.ts
+++ src/player.ts
@@ -1,13 +1,19 @@
 export class Player {
     x: number;
     y: number;
-    w: number;
-    h: number;
+    readonly w: number;
+    readonly h: number;
+    falling: boolean;
+    jumping: boolean;
 
     constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
         this.w = 20;
         this.h = 60;
+        this.falling = false;
+        this.jumping = false;
     }
+
+
 }
